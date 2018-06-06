8de217840b866ded497f2ca45891f90a330a7e8d
Mat Booth <mat.booth@redhat.com>
2018-06-06 14:49:08 +0100

    Refactored

--- com.example.TestJava/build.sh
+++ com.example.TestJava/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 flatpak update
-flatpak-builder --force-clean --repo=repo build org.example.TestJava.json
+flatpak-builder --force-clean --repo=repo build com.example.TestJava.json
 flatpak remote-add --if-not-exists --no-gpg-verify test-java file://$(pwd)/repo
