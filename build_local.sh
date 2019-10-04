0fd7157b30f9e6677385aeb6755d49d7c4e9683b
Mat Booth <mat.booth@redhat.com>
2019-10-04 17:32:48 +0100

    Update Java Latest extension

--- build_local.sh
+++ build_local.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 for jdk in $* ; do
-	flatpak-builder --force-clean --repo=repo jdk-build org.freedesktop.Sdk.Extension.openjdk$jdk/org.freedesktop.Sdk.Extension.openjdk$jdk.json
+	flatpak run org.freedesktop.appstream-glib validate org.freedesktop.Sdk.Extension.$jdk/*.xml
+	flatpak-builder --force-clean --repo=repo jdk-build org.freedesktop.Sdk.Extension.$jdk/org.freedesktop.Sdk.Extension.$jdk.json
 	flatpak remote-add --if-not-exists --no-gpg-verify test-java file://$(pwd)/repo
-	flatpak install -y test-java org.freedesktop.Sdk.Extension.openjdk$jdk
-	flatpak update -y org.freedesktop.Sdk.Extension.openjdk$jdk
+	flatpak install -y test-java org.freedesktop.Sdk.Extension.$jdk
+	flatpak update -y org.freedesktop.Sdk.Extension.$jdk
 done
 
 flatpak-builder --force-clean --repo=repo jdk-build com.example.TestJava/com.example.TestJava.json
+flatpak remote-add --if-not-exists --no-gpg-verify test-java file://$(pwd)/repo
 flatpak install -y test-java com.example.TestJava
 flatpak update -y com.example.TestJava
